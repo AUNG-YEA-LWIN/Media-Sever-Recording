f87ad849c920aaeed3659dad4680436028df5c90
AUNG-YEA-LWIN <aungyealwin2021@gmail.com>
2022-07-21 01:07:46 +0630

    home.html file read and update

--- index.js
+++ index.js
@@ -9,16 +9,23 @@ let responder = (req, res, params) => {
 };
 let routes = {
   'GET': {
-    '/': (req, res, params) => {
-      responder(
-        req,
-        res,
-        `<h1>Method GET => path / and Name = ${params.query.name} and age = ${params.query.age} </h1>`
-      );
+    '/': (req,res) => {
+      let filepath = __dirname+"/index.html";
+      responder(req,res,filepath);
     },
     '/home': (req, res) => {
-      responder(req,res,'<h1>Method GET => path /home</h1>');
+      let filepath = __dirname+"/index.html";
+      responder(req,res,filepath);
+    }, 
+    '/index': (req,res) => {
+      let filepath = __dirname+"/index.html";
+      responder(req,res,filepath);
     },
+    '/about': (req,res) => {
+      let filepath = __dirname+"/about.html";
+      responder(req,res,filepath);
+    }
+    
   },
   'POST': {
     '/': (req, res) => {
@@ -28,6 +35,10 @@ let routes = {
         let body = '';
         req.on('data',data  => {
             body += data;
+            if(body.length > 1024) {
+              res.writeHead(403,{"Content-Type": 'text/html'});
+              res.end("<h1>File size is over 1MB</h1>")
+            }
         });
         req.on('end',() => {
             let query = qs.parse(body);
